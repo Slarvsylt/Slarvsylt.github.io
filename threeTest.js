1d6dff2f822a779628fd49e7b23ffa43f30755d1
Jesper Berglind <jesper.berglind@gmail.com>
2019-04-10 07:58:12 +0200

    Looks cool

--- threeTest.js
+++ threeTest.js
@@ -1,20 +1,78 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-camera.position.set(5,5,0);
-camera.lookAt(new THREE.Vector3(0,0,0));
-renderer.setSize(window.innerWidth/2,window.innerHeight/2);
-renderer.setClearColor(0xfff6e6);
-document.body.appendChild(renderer.domElement);
+var camera, scene, renderer;
+var mesh;
+init();
+animate();
 
-var plane = new THREE.Mesh(
-  new THREE.PlaneGeometry( 5, 5, 5, 5 ),
-  new THREE.MeshBasicMaterial( { color: 0x393839, wireframe: true } )
-);
-plane.rotateX(Math.PI/2);
-scene.add( plane );
+function init(){
 
-renderer.render(scene, camera);
+	var ASPECT_RATIO = window.innerWidth / window.innerHeight;
+	var AMOUNT = 6;
+	var WIDTH = ( window.innerWidth / AMOUNT ) * window.devicePixelRatio;
+  var HEIGHT = ( window.innerHeight / AMOUNT ) * window.devicePixelRatio;
+  
+  var cameras = [];
+  
+	for ( var y = 0; y < AMOUNT; y ++ ) {
+		for ( var x = 0; x < AMOUNT; x ++ ) {
+			var subcamera = new THREE.PerspectiveCamera( 40, ASPECT_RATIO, 0.1, 10 );
+			subcamera.viewport = new THREE.Vector4( Math.floor( x * WIDTH ), Math.floor( y * HEIGHT ), Math.ceil( WIDTH ), Math.ceil( HEIGHT ) );
+			subcamera.position.x = ( x / AMOUNT ) - 0.5;
+			subcamera.position.y = 0.5 - ( y / AMOUNT );
+			subcamera.position.z = 1.5;
+			subcamera.position.multiplyScalar( 2 );
+			subcamera.lookAt( 0, 0, 0 );
+			subcamera.updateMatrixWorld();
+			cameras.push( subcamera );
+		}
+  }
+  
+	camera = new THREE.ArrayCamera( cameras );
+  camera.position.z = 3;
+  
+	scene = new THREE.Scene();
+  scene.add( new THREE.AmbientLight( 0x222244 ) );
+  
+	var light = new THREE.DirectionalLight();
+	light.position.set( 1.5, 0.5, 1 );
+	light.castShadow = true;
+	light.shadow.camera.zoom = 4; // tighter shadow map
+  scene.add( light );
+  
+	var geometry = new THREE.PlaneBufferGeometry( 100, 100 );
+  var material = new THREE.MeshPhongMaterial( { color: 0x669966 } );
+  
+  var background = new THREE.Mesh( geometry, material );
+	background.receiveShadow = true;
+	background.position.set( 0, 0, - 1 );
+  scene.add( background );
+  
+	var geometry = new THREE.OctahedronBufferGeometry( 1, 1 );
+  var material = new THREE.MeshStandardMaterial( { color: 0xfffe66, shading: THREE.FlatShading, metalness: 0, roughness: 0.9 } );
+  
+	mesh = new THREE.Mesh( geometry, material );
+	mesh.castShadow = true;
+	mesh.receiveShadow = true;
+  scene.add( mesh );
+  
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  
+	document.body.appendChild( renderer.domElement );
+	//
+	window.addEventListener( 'resize', onWindowResize, false );
+}
 
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.addEventListener( 'change', function() { renderer.render(scene, camera); } );
+function onWindowResize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate(){
+	mesh.rotation.x += 0.005;
+	mesh.rotation.z += 0.01;
+	renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+}
