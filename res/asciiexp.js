5e4e77eba1358065d3ceaca61d8df399f301fdaf
Slarvsylt <jesper.berglind@gmail.com>
2022-11-11 14:03:07 +0100

    weird

--- res/asciiexp.js
+++ res/asciiexp.js
@@ -6,6 +6,8 @@
 
 import * as v2 from '/src/modules/vec2.js'
 import * as v3 from '/src/modules/vec3.js'
+import { map } from '/src/modules/num.js'
+import { css } from '/src/modules/color.js'
 
 const pointer = {
 	x        : 0,
@@ -46,7 +48,17 @@ window.setInterval(() => {
 }, 1000 / 60);
 
 // '█8AOuoi=-;:,.'
-const pattern = '█8#Ouoi=-;:,.'
+const pattern = '01#Ouoi=-;:,.'
+const palette = [
+	'black',        // 0 < top
+	'purple',       // 1
+	'darkred',      // 2
+	'red',          // 3
+	'orangered',    // 4
+	'gold',         // 5
+	'lemonchiffon', // 6
+	'white'         // 7 < bottom
+]
 
 // This is the main loop.
 // Character coordinates are passed in coord {x, y, index}.
@@ -65,18 +77,24 @@ export function main(coord, context, cursor, buffer) {
 	const px = l.x/res.y
 	const py = l.y/res.y
 	const a = Math.atan(px,py)
-	const r = Math.pow(Math.pow(px*px,1.5) + Math.pow(py*py,1), 1/6)
-	const u = 1/r + 0.2*t
+	const r = Math.pow(Math.pow(px*px,1.5) + Math.pow(py*py,0.5), 1/3)
+	const u = 1/r*2 + 0.2*t
 	const f = Math.cos(12*u) * Math.cos(9*a)
-	var col = 0.3 + Math.sin(3.1415*f * 1.5)
-	col *= r
+	//var col = 0.3 + Math.sin(3.1415*f * 1.5)
+	const col1 = map((0.3 + 0.5*Math.sin(3.1415*f * 0)),-0.2,0.8,0,255)*r
+	const col2 = map((0.3 + 0.5*Math.sin(3.1415*f * 0.5)),-0.2,0.8,0,255)*r
+	const col3 = map((0.3 + 0.5*Math.sin(3.1415*f * 1.0)),-0.2,0.8,0,255)*r
+	//var colr = v3.vec3(col1,col2,col3)
+	//col *= r
 	//const i = Math.round(col) % pattern.length
-	//console.log(i)
-	const o = Math.sin(y * Math.sin(t) * 0.2 + x * 0.04 + t) 
-	const i = Math.round(Math.abs(col + o)) % pattern.length
+	//console.log(col1+' '+col2+' '+col3)
+	//const o = Math.sin(y * Math.sin(t) * 0.2 + x * 0.04 + t) 
+	//const i = Math.round(Math.abs(col + o)) % pattern.length
 	return {
-		char   : pattern[i],
-		color   : 'blue',
+		char   : '.',
+		color   : 'black',
+		backgroundColor : css(col1,col2,col3), 
+		//backgroundColor : 'rgb(89,100,0)', 
 		fontWeight : 'light', // or 'light', 'bold', '400'
 	}
 }
