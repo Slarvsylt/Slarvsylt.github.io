31f3c085ee9eacee2fda97de60d0e557e930171d
Slarvsylt <jesper.berglind@gmail.com>
2022-11-16 16:07:18 +0100

    saas

--- res/asciiexp.js
+++ res/asciiexp.js
@@ -66,20 +66,27 @@ const palette = [
 // {char, color, background, weight}.
 export function main(coord, context, cursor, buffer) {
 
-	const res = v2.vec2(context.cols, context.rows)
+	const pi = v2.vec2(context.cols, context.rows)
+	const coords = v2.vec2(coord.x*2, coord.y*2)
+	const res = v2.add(v2.neg(pi) , coords)
+	const res2 = v2.vec2(res.x/pi.y,res.y/pi.y)
+	//console.log(res2)
 
 	const t = context.time * 0.001
 	const x = coord.x
 	const y = coord.y
 
-	const xy = 	v2.vec2(2*x,2*y)
-	const l = v2.add(v2.neg(res),xy)
-	const px = l.x/res.y
-	const py = l.y/res.y
-	const a = Math.atan(px,py)
-	const r = Math.pow(Math.pow(px*px,1.5) + Math.pow(py*py,0.5), 1/3)
+	//const xy = 	v2.vec2(2*x,2*y)
+	//const l = v2.add(v2.neg(res2),xy)
+	//const px = l.x/res2.y
+	//const py = l.y/res2.y
+	const px = res2.x
+	const py = res2.y
+	const a = Math.atan(py,px)
+	const r = Math.pow(Math.pow(px*px,2.5) + Math.pow(py*py,1.5), 1/5)
 	const u = 1/r*2 + 0.2*t
-	const f = Math.cos(12*u) * Math.cos(9*a)
+	const uv = v2.vec2(u,a)
+	const f = Math.cos(12*u) * Math.cos(6*a)	
 	//var col = 0.3 + Math.sin(3.1415*f * 1.5)
 	const col1 = map((0.3 + 0.5*Math.sin(3.1415*f * 0)),-0.2,0.8,0,255)*r
 	const col2 = map((0.3 + 0.5*Math.sin(3.1415*f * 0.5)),-0.2,0.8,0,255)*r
@@ -92,7 +99,7 @@ export function main(coord, context, cursor, buffer) {
 	//const i = Math.round(Math.abs(col + o)) % pattern.length
 	return {
 		char   : '.',
-		color   : 'black',
+		color   : css(col1,col2,col3),
 		backgroundColor : css(col1,col2,col3), 
 		//backgroundColor : 'rgb(89,100,0)', 
 		fontWeight : 'light', // or 'light', 'bold', '400'
