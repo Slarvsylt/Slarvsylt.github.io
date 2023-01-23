f7cd1e57817394afaa1e5943f9c5486cfcb4d6d4
Slarvsylt <jesper.berglind@gmail.com>
2023-01-23 14:03:56 +0100

    faces

--- res/coolPhysics.js
+++ res/coolPhysics.js
@@ -2,21 +2,45 @@
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Composite = Matter.Composite,
+    Bodies = Matter.Bodies;
 
 // create an engine
-var engine = Engine.create();
+var engine = Engine.create(),
+    world = engine.world;
 
 // create a renderer
 var render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+    options: {
+        wireframes: false
+    }
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+var boxA = Bodies.rectangle(400, 200, 80, 80, {
+    render:{
+        sprite:{
+            texture:'../pics/jeppeFace.jpg',
+            yScale: 0.27,
+            xScale: 0.27
+        }
+    }
+});
+var boxB = Bodies.rectangle(450, 50, 80, 80, {
+    render:{
+        sprite:{
+            texture:'../pics/yokoFace.jpg',
+            yScale: 0.27,
+            xScale: 0.27
+        }
+    }
+});
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
